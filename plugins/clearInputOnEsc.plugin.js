982d11951ffc9e93df5fe6d97bc03600bb081251
Inve1951 <Inve1951@jourrapide.com>
2017-06-21 22:40:40 +0200

    first commit

---
+++ plugins/clearInputOnEsc.plugin.js
@@ -0,0 +1,22 @@
+//META{"name":"clearInputOnEsc"}*//
+
+class clearInputOnEsc {
+	getName(){return "Clear-Input-on-Escape"}
+	getDescription(){return "Clears the chat input when you hit escape inside it."}
+	getVersion(){return "1.0.0"}
+	getAuthor(){return "square"}
+	
+	load(){}
+	unload(){}
+	
+	start(){
+		document.addEventListener("keydown", this.listener = function(e){
+			if("Escape" === e.key && document.activeElement === document.querySelector(".channel-textarea textarea"))
+				$(document.activeElement).val("")
+		})
+	}
+	
+	stop(){
+		document.removeEventListener("keydown", this.listener)
+	}
+}
---
+++ plugins/forceClose.plugin.js
@@ -0,0 +1,22 @@
+//META{"name":"forceClose"}*//
+function forceClose() {};
+
+forceClose.prototype.getName = function(){ return "Force-Close" };
+forceClose.prototype.getDescription = function(){ return "Actually closes discord when clicking the close button." };
+forceClose.prototype.getVersion = function(){ return "1.0.0" };
+forceClose.prototype.getAuthor = function(){ return "square" };
+
+forceClose.prototype.start = function(){
+	document.querySelector(".win-close").onclick = function(){
+		require("electron").remote.require("electron").app.quit();
+	};
+};
+
+forceClose.prototype.stop = function(){
+	document.querySelector(".win-close").onclick = void 0;
+};
+
+forceClose.prototype.load =
+forceClose.prototype.unload =
+forceClose.prototype.onSwitch =
+forceClose.prototype.observer = function(){};
