5a2caf71fdc24a233992518cc37495f93989d1af
Inve1951 <Inve1951@jourrapide.com>
2017-06-23 06:17:50 +0200

    no jQuery

--- plugins/clearInputOnEsc.plugin.js
+++ plugins/clearInputOnEsc.plugin.js
@@ -3,7 +3,7 @@
 class clearInputOnEsc {
 	getName(){return "Clear-Input-on-Escape"}
 	getDescription(){return "Clears the chat input when you hit escape inside it."}
-	getVersion(){return "1.0.0"}
+	getVersion(){return "1.0.1"}
 	getAuthor(){return "square"}
 	
 	load(){}
@@ -12,7 +12,7 @@ class clearInputOnEsc {
 	start(){
 		document.addEventListener("keydown", this.listener = function(e){
 			if("Escape" === e.key && document.activeElement === document.querySelector(".channel-textarea textarea"))
-				$(document.activeElement).val("")
+				document.activeElement.value = ""
 		})
 	}
 	
