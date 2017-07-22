a642ef8a95714644ee5a7cf83e4401b8edcaaab3
Inve1951 <Inve1951@jourrapide.com>
2017-07-22 03:11:13 +0200

    clearInputOnEscape v1.1.0 fixed and improved

--- plugins/clearInputOnEsc.plugin.js
+++ plugins/clearInputOnEsc.plugin.js
@@ -3,7 +3,7 @@
 class clearInputOnEsc {
 	getName(){return "Clear-Input-on-Escape"}
 	getDescription(){return "Clears the chat input when you hit escape inside it."}
-	getVersion(){return "1.0.1"}
+	getVersion(){return "1.1.0"}
 	getAuthor(){return "square"}
 	
 	load(){}
@@ -11,12 +11,13 @@ class clearInputOnEsc {
 	
 	start(){
 		document.addEventListener("keydown", this.listener = function(e){
-			if("Escape" === e.key && document.activeElement === document.querySelector(".channel-textarea textarea"))
-				document.activeElement.value = ""
+			if("Escape" === e.key && document.activeElement === document.querySelector(".content textarea"))
+				document.activeElement.value = "";
+				document.activeElement.dispatchEvent(new Event("input", { bubbles: true }));
 		})
 	}
 	
 	stop(){
-		document.removeEventListener("keydown", this.listener)
+		document.removeEventListener("keydown", this.listener);
 	}
 }
