42d20606c5f8add7f4c08ada96a34298e3e6c2bf
Inve1951 <Inve1951@jourrapide.com>
2017-07-28 23:16:21 +0200

    directDownload alpha 2

--- plugins/directDownload.plugin.js
+++ plugins/directDownload.plugin.js
@@ -2,7 +2,7 @@
 var directDownload;
 
 directDownload = (function() {
-  var download, fs, getSettings, https, installListener, installObserver, obs, path, settings;
+  var Download, download, fs, getSettings, https, installCss, installDownloadBar, installListener, installObserver, obs, path, settings, shell;
 
   class directDownload {
     getName() {
@@ -18,11 +18,13 @@ directDownload = (function() {
     }
 
     getVersion() {
-      return "0.0.1-alpha";
+      return "0.0.2-alpha";
     }
 
     start() {
       getSettings();
+      installCss();
+      installDownloadBar();
       installObserver();
     }
 
@@ -32,6 +34,8 @@ directDownload = (function() {
 
     stop() {
       var i, len, ref, x;
+      document.getElementById("files_directDownload").remove();
+      document.getElementById("css_directDownload").remove();
       if (typeof obs !== "undefined" && obs !== null) {
         obs.disconnect();
       }
@@ -46,7 +50,7 @@ directDownload = (function() {
 
     getSettingsPanel() {
       getSettings();
-      return `<div id=\"settings_directDownload\" style=\"color:#87909C\">\n  <span style=\"display:block;-webkit-user-select:text;height:1.2em\">Last selected: ${settings.dldir}</span>\n  <input name=\"dldir\" type=\"file\" style=\"width:100%\" webkitdirectory onchange=\"directDownload.updateSettings()\"></input>\n</div>`;
+      return `<div id=\"settings_directDownload\" style=\"color:#87909C\">\n  <span style=\"display:block;-webkit-user-select:text;height:1.2em\">Last selected: ${settings.dldir}</span>\n  <input name=\"dldir\" type=\"file\" webkitdirectory onchange=\"directDownload.updateSettings()\"></input>\n  <label><input name=\"autoopen\" type=\"checkbox\" ${(settings.autoopen ? "checked" : "")} onchange=\"directDownload.updateSettings()\"/>Open files after download.</label>\n  <label><input name=\"showinstead\" type=\"checkbox\" ${(settings.showinstead ? "checked" : "")} onchange=\"directDownload.updateSettings()\"/>Show in folder instead.</label>\n</div>`;
     }
 
     static updateSettings() {
@@ -58,12 +62,17 @@ directDownload = (function() {
         if (type === "file") {
           value = (ref1 = input.files[0]) != null ? ref1.path : void 0;
         }
-        if (value && (function() {
+        if (type === "checkbox") {
+          value = input.checked;
+        }
+        if (((function() {
           switch (name) {
             case "dldir":
-              return (path.isAbsolute(value)) && fs.existsSync(value);
+              return value && (path.isAbsolute(value)) && fs.existsSync(value);
+            default:
+              return true;
           }
-        })()) {
+        })())) {
           settings[name] = value;
           input.style = "";
         } else {
@@ -75,11 +84,30 @@ directDownload = (function() {
 
   };
 
+  installCss = function() {
+    var style;
+    style = document.createElement("style");
+    style.id = "css_directDownload";
+    style.innerHTML = Download.css;
+    document.head.appendChild(style);
+  };
+
+  installDownloadBar = function() {
+    var div, style;
+    div = document.createElement("div");
+    div.id = "files_directDownload";
+    style = document.createElement("style");
+    div.appendChild(style);
+    (document.querySelector(".app .layers .layer section")).appendChild(div);
+  };
+
   fs = require("fs");
 
   https = require("https");
 
   path = require("path");
+
+  ({shell} = (require("electron")).remote.require("electron"));
 
   settings = {};
 
@@ -117,9 +145,6 @@ directDownload = (function() {
         var addedNodes, i, icon, j, l, len, len1, len2, node, ref;
         for (i = 0, len = mutations.length; i < len; i++) {
           ({addedNodes} = mutations[i]);
-          if (addedNodes.length == null) {
-            addedNodes = [addedNodes];
-          }
           for (j = 0, len1 = addedNodes.length; j < len1; j++) {
             node = addedNodes[j];
             ref = node.querySelectorAll(".attachment > .icon-file");
@@ -131,7 +156,7 @@ directDownload = (function() {
         }
       })([
         {
-          addedNodes: chat
+          addedNodes: [chat]
         }
       ]);
       obs = new MutationObserver(obs);
@@ -143,37 +168,9 @@ directDownload = (function() {
 
   download = function(elem) {
     return elem.dl = function(event) {
-      var a, buffer, bufpos, filename, filepath, filesize, req, url;
-      url = (a = elem.querySelector("a")).href;
-      filename = a.innerHTML;
-      filepath = path.join(settings.dldir, filename);
-      filesize = bufpos = 0;
-      req = buffer = a = null;
-      req = https.get(url, function(res) {
-        filesize = 0 | res.headers["content-length"];
-        buffer = Buffer.alloc(filesize);
-        res.on("data", function(chunk) {
-          chunk.copy(buffer, bufpos);
-          bufpos += Buffer.byteLength(chunk);
-        });
-        return res.on("end", function() {
-          if (filesize !== bufpos) {
-            console.log(`Download failed for ${filename}: ${filesize}bytes announced, ${bufpos}bytes received!`);
-            return;
-          }
-          fs.writeFile(filepath, buffer, function(error) {
-            if (error) {
-              console.log(error);
-              return;
-            }
-            return console.log(`File saved to ${filepath}.`);
-          });
-        });
-      });
-      req.on("error", function(error) {
-        console.log(error);
-      });
-      req.end();
+      if (elem.inProcess == null) {
+        elem.inProcess = new Download(elem);
+      }
       event.preventDefault();
       return false;
     };
@@ -183,7 +180,9 @@ directDownload = (function() {
     var k, ref, ref1, v;
     settings = (ref = bdPluginStorage.get("directDownload", "settings")) != null ? ref : {};
     ref1 = {
-      dldir: path.join(process.env[process.platform === "win32" ? "USERPROFILE" : "HOME"], "downloads")
+      dldir: path.join(process.env[process.platform === "win32" ? "USERPROFILE" : "HOME"], "downloads"),
+      autoopen: false,
+      showinstead: false
     };
     for (k in ref1) {
       v = ref1[k];
@@ -192,6 +191,143 @@ directDownload = (function() {
       }
     }
   };
+
+  Download = (function() {
+    class Download {
+      constructor(att) {
+        var a, buffer, bufpos, elem, req, url;
+        this.att = att;
+        this.filename = this.filepath = "";
+        this.filesize = bufpos = 0;
+        buffer = elem = this.pb = null;
+        this.started = this.finished = this.failed = false;
+        this.openWhenFinished = settings.autoopen;
+        this.showinstead = settings.showinstead;
+        url = (a = this.att.querySelector("a")).href;
+        this.filename = a.innerHTML;
+        this.filepath = path.join(settings.dldir, this.filename);
+        this.filesize = this.bufpos = 0;
+        req = this.buffer = a = null;
+        this.start();
+        req = https.get(url, (res) => {
+          if (200 !== res.statusCode) {
+            this.fail();
+            console.error(`Download failed for ${this.filename} with code ${res.statusCode}:\n${res.statusMessage}`);
+            return;
+          }
+          this.filesize = 0 | res.headers["content-length"];
+          this.buffer = Buffer.alloc(this.filesize);
+          this.progress();
+          res.on("data", (chunk) => {
+            chunk.copy(this.buffer, this.bufpos);
+            this.bufpos += Buffer.byteLength(chunk);
+            this.progress();
+          });
+          return res.on("end", () => {
+            if (this.filesize !== this.bufpos) {
+              console.error(`Download failed for ${this.filename}: ${this.filesize}bytes announced, ${this.bufpos}bytes received!`);
+              this.fail();
+              return;
+            }
+            this.finish();
+          });
+        });
+        req.on("error", (error) => {
+          this.fail();
+          console.error(error);
+        });
+        req.end();
+      }
+
+      start() {
+        var span, svg;
+        this.started = true;
+        this.elem = document.createElement("div");
+        this.elem.className = "file";
+        this.elem.innerHTML = `<span>${this.filename}</span>\n<svg viewBox=\"0 0 26 26\">\n    <path d=\"M20 7.41L18.59 6 13 11.59 7.41 6 6 7.41 11.59 13 6 18.59 7.41 20 13 14.41 18.59 20 20 18.59 14.41 13 20 7.41z\"/>\n</svg>`;
+        this.pb = document.createElement("div");
+        this.pb.className = "progress-bar";
+        this.elem.appendChild(this.pb);
+        span = this.elem.querySelector("span");
+        svg = this.elem.querySelector("svg");
+        span.onclick = (event) => {
+          if (this.finished) {
+            this.open();
+          } else {
+            this.elem.classList.toggle("will-open", (this.openWhenFinished = !this.openWhenFinished));
+          }
+          event.preventDefault();
+          return false;
+        };
+        span.oncontextmenu = (event) => {
+          if (this.finished) {
+            this.show();
+          }
+          event.preventDefault();
+          return false;
+        };
+        svg.onclick = (event) => {
+          this.elem.remove();
+          Download.updateFileWidth();
+          event.preventDefault();
+          return false;
+        };
+        (document.getElementById("files_directDownload")).appendChild(this.elem);
+        Download.updateFileWidth();
+      }
+
+      progress() {
+        this.pb.style = `width: calc((100% + 2px) * ${this.bufpos / this.filesize});`;
+      }
+
+      finish() {
+        fs.writeFile(this.filepath, this.buffer, (error) => {
+          delete this.att.inProcess;
+          if (error) {
+            this.fail();
+            console.error(error);
+            return;
+          }
+          this.elem.classList.remove("will-open");
+          this.elem.classList.add("done");
+          this.finished = true;
+          console.log(`File saved to ${this.filepath}.`);
+          if (this.openWhenFinished) {
+            if (!this.showinstead) {
+              this.open();
+            } else {
+              this.show();
+            }
+          }
+        });
+      }
+
+      fail() {
+        this.failed = true;
+        this.elem.classList.add("failed");
+      }
+
+      open() {
+        shell.openItem(this.filepath);
+      }
+
+      show() {
+        shell.showItemInFolder(this.filepath);
+      }
+
+      static updateFileWidth() {
+        var numFiles;
+        numFiles = (document.querySelectorAll("#files_directDownload .file")).length;
+        (document.querySelector("#files_directDownload style")).innerHTML = `#files_directDownload .file{max-width: calc((100% + 2px) / ${numFiles} - 2px);}`;
+      }
+
+    };
+
+    Download.css = "#files_directDownload {\n  position: fixed;\n  bottom: 0;\n  left: 310px;\n  width: calc(100% - 310px - 240px);\n  height: 25px;\n  overflow: hidden;\n  font-size: 0;\n}\n.bd-minimal #files_directDownload {\n  left: 275px;\n  width: calc(100% - 275px - 185px);\n}\n#files_directDownload .file {\n  height: 100%;\n  width: 200px;\n  min-width: 50px;\n  background: rgba(128,128,128,0.2);\n  display: inline-block;\n  margin-left: 2px;\n  box-shadow: inset 0 0 10px rgba(0,0,0,0.3);\n  border: 1px solid rgba(128,128,128,0.2);\n  border-bottom: none;\n  box-sizing: border-box;\n  position: relative;\n  cursor: pointer;\n}\n#files_directDownload .file:first-of-type {\n  border-top-left-radius: 4px;\n  margin: 0;\n}\n#files_directDownload .file:last-of-type {\n  border-top-right-radius: 4px;\n}\n#files_directDownload .file.done {\n  box-shadow: none;\n}\n#files_directDownload .file.will-open {\n  background: rgba(128,128,128,0.4);\n}\n#files_directDownload span {\n  width: calc(100% + 2px);\n  overflow: hidden;\n  text-overflow: ellipsis;\n  color: #87909C;\n  /*display: inline-block;*/\n  position: absolute;\n  left: -1px;\n  top: -1px;\n  font-size: 14px;\n  line-height: 23px;\n  padding: 0 18px 0 4px;\n  box-sizing: border-box;\n}\n#files_directDownload .file .progress-bar {\n  position:absolute;\n  height: 2px;\n  bottom: 0;\n  left: -1px;\n  background: rgb(32,196,64);\n}\n#files_directDownload .file.failed .progress-bar {\n  background: rgb(196,64,32);\n  min-width: calc(100% + 2px);\n}\n#files_directDownload .file svg {\n  fill: rgba(0,0,0,0.5);\n  position: absolute;\n  top: -1px;\n  right: -1px;\n  height: 23px;\n  width: 23px;\n}\n\n#settings_directDownload :-webkit-any(label, input) {\n  cursor: pointer;\n}\n#settings_directDownload [type=\"file\"] {\n  width: 100%;\n}\n\n.attachment {\n  cursor: pointer;\n}";
+
+    return Download;
+
+  })();
 
   return directDownload;
 
