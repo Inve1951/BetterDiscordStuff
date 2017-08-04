cd1517ab9db2af94e2b0b3e555ba7603ee26081c
Inve1951 <Inve1951@jourrapide.com>
2017-08-04 13:58:19 +0200

    hover fix 1.0.2

--- plugins/autocompleteHoverFix.plugin.js
+++ plugins/autocompleteHoverFix.plugin.js
@@ -15,7 +15,7 @@ autocompleteHoverFix = class autocompleteHoverFix {
   }
 
   getVersion() {
-    return "1.0.1";
+    return "1.0.2";
   }
 
   start() {}
@@ -25,7 +25,7 @@ autocompleteHoverFix = class autocompleteHoverFix {
   load() {}
 
   observer(m) {
-    var e, node;
+    var e, node, ref;
     try {
       ({
         addedNodes: [node]
@@ -34,14 +34,17 @@ autocompleteHoverFix = class autocompleteHoverFix {
       e = error;
       return;
     }
-    if (((node != null ? node.className : void 0) != null) && ((-1 !== node.className.indexOf("autocomplete-")) || -1 !== node.className.indexOf("popout-"))) {
+    if ("function" === typeof (node != null ? (ref = node.className) != null ? ref.indexOf : void 0 : void 0) && ((-1 !== node.className.indexOf("autocomplete-")) || -1 !== node.className.indexOf("popout-"))) {
       e = document.createElement("div");
       e.style = "position: absolute;\nwidth: 100%;\nheight: 100%;\ntop: 0; left: 0;";
       e.onmousemove = function({clientX, clientY}) {
         if ((this.x != null) && (this.y != null) && (this.x !== clientX || this.y !== clientY)) {
           this.remove();
           e = document.elementFromPoint(clientX, clientY);
-          while (e !== document.body) {
+          while (true) {
+            if (e === document.body) {
+              return;
+            }
             if (-1 !== e.className.indexOf("selectable-")) {
               e = e.parentNode;
               break;
@@ -49,9 +52,6 @@ autocompleteHoverFix = class autocompleteHoverFix {
               break;
             }
             e = e.parentNode;
-          }
-          if (e === document.body) {
-            return;
           }
           e.dispatchEvent(new MouseEvent("mouseover", {
             clientX,
