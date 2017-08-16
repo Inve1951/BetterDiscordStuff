9ed696215a4e8bad8c3c5585c80f8b70c69335c4
Inve1951 <Inve1951@jourrapide.com>
2017-08-16 17:24:13 +0200

    react devtools 0.0.2

--- plugins/enableReactDevtools.plugin.js
+++ plugins/enableReactDevtools.plugin.js
@@ -1,13 +1,13 @@
 //META { "name": "enableReactDevtools" } *//
 
 var enableReactDevtools = (function(){
-  var listener, bw;
+  var listener, bw, versions, path;
 
   class enableReactDevtools {
     getName() { return "Enable React-Devtools" }
-    getDescription() { return "Automatically loads the React Devtools for you." }
+    getDescription() { return "Automatically loads the React Devtools for you. Big thanks to Natsulus who helped figgure this out!" }
     getAuthor() { return "square" }
-    getVersion() { return "0.0.1" }
+    getVersion() { return "0.0.2" }
 
     start(){
       bw = require("electron").remote.BrowserWindow.getAllWindows()[0];
@@ -21,14 +21,23 @@ var enableReactDevtools = (function(){
     load(){}
   }
 
+  try {
+    path = require("path").resolve(
+      process.env.LOCALAPPDATA, "Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/"
+    )
+    versions = require("fs").readdirSync( path );
+    path = require("path").resolve( path, versions[versions.length - 1] )
+  } catch (e) {
+    path = null
+    if (e !== "ENOENT")
+      throw e;
+    console.error("Couldn't find react devtools in chrome extensions!");
+  }
+
   listener = function(){
     require("electron").remote.BrowserWindow.removeDevToolsExtension("React Developer Tools");
     require("electron").webFrame.registerURLSchemeAsSecure("chrome-extension");
-    var success = require("electron").remote.BrowserWindow.addDevToolsExtension(
-      process.env.localappdata +
-      "\\Google\\Chrome\\User Data\\Default\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi\\2.5.0_0"
-    );
-    if(success != null)
+    if( path != null && null != require("electron").remote.BrowserWindow.addDevToolsExtension( path ) )
       console.log("Successfully installed react devtools.");
     else
       console.error("Couldn't find react devtools in chrome extensions!");
