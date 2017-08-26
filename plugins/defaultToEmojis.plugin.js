0e4ff0a1181aaefaf02c81301782b3315942d817
Inve1951 <Inve1951@jourrapide.com>
2017-08-26 15:15:42 +0200

    defaultToEmojis indents

--- plugins/defaultToEmojis.plugin.js
+++ plugins/defaultToEmojis.plugin.js
@@ -1,24 +1,24 @@
 //META{"name":"defaultToEmojis"}*//
 
 var defaultToEmojis = (function(){
-	var n, i;
-	class defaultToEmojis {
-		getName(){return "Default To Emojis"}
-		getDescription(){return "Selects the emoji tab first time you open the picker."}
-		getVersion(){return "0.0.1"}
-		getAuthor(){return "square"}
+  var n, i;
+  class defaultToEmojis {
+    getName(){return "Default To Emojis"}
+    getDescription(){return "Selects the emoji tab first time you open the picker."}
+    getVersion(){return "0.0.1"}
+    getAuthor(){return "square"}
 
-		start(){}
-		stop(){}
-		load(){}
+    start(){}
+    stop(){}
+    load(){}
 
-		observer({addedNodes}){
-			for(i = 0; n = addedNodes[i]; i++) if(n.classList && n.classList.contains("popout") && (n = document.getElementById("bda-qem-emojis"))) {
-				n.click();
-				delete this.observer;
-				return;
-			}
-		}
-	}
-	return defaultToEmojis;
+    observer({addedNodes}){
+      for(i = 0; n = addedNodes[i]; i++) if(n.classList && n.classList.contains("popout") && (n = document.getElementById("bda-qem-emojis"))) {
+        n.click();
+        delete this.observer;
+        return;
+      }
+    }
+  }
+  return defaultToEmojis;
 })()
