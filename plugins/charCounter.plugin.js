f79a7c1b3ed45c2e168e20447ee2efb12a4a70f0
Inve1951 <Inve1951@jourrapide.com>
2017-11-02 02:07:37 +0100

    charCounter 1.0.2

--- plugins/charCounter.plugin.js
+++ plugins/charCounter.plugin.js
@@ -1,7 +1,9 @@
 //META { "name": "charCounter" } *//
 
 var charCounter = (function (){
-  var injectCss, inject;
+  var injectCss, inject, initObserver, switchObserver, some;
+
+  some = Array.prototype.some;
 
   class charCounter {
     getName() { return "Character Counter"; }
@@ -10,18 +12,20 @@ var charCounter = (function (){
 
     getAuthor() { return "Jiiks, square"; };
 
-    getVersion() { return "1.0.1"; }
+    getVersion() { return "1.0.2"; }
 
     load(){}
 
     start() {
       injectCss();
       inject();
+      initObserver();
     }
 
     stop() {
       $(".content textarea").off("keyup.charcounter");
       BdApi.clearCSS("charCounter");
+      switchObserver.disconnect();
     }
 
     onSwitch() {
@@ -51,6 +55,19 @@ var charCounter = (function (){
       $("#charcounter").text(`${e.target.value.length}/2000`)
     );
   };
+
+  initObserver = function() {
+    var target;
+    switchObserver = new MutationObserver(function(mutations) {
+      if(some.call(mutations, function({addedNodes}) {
+        return some.call(addedNodes, function(node) {
+          return node.classList != null && (node.classList.contains("chat") || node.classList.contains("messages-wrapper"));
+        });
+      })) inject();
+    });
+    if((target = document.querySelector("#friends, .chat, .activityFeed-HeiGwL")) != null)
+      switchObserver.observe(target.parentNode, {childList: true, subtree: true});
+  }
 
   return charCounter;
 })();
