ef57269fac2510b614b4c70f24ab853474afb6e6
Inve1951 <Inve1951@jourrapide.com>
2017-11-04 15:40:19 +0100

    scrollToLast 0.0.3

--- plugins/scrollToLast.plugin.js
+++ plugins/scrollToLast.plugin.js
@@ -1,21 +1,41 @@
 //META { "name": "scrollToLast" } *//
 
-var scrollToLast = (function(){
+var scrollToLast = (function(some, initObserver, switchObserver, onSwitch, ev){
+
+  some = Array.prototype.some;
+
   class scrollToLast {
     getName() { return "Scroll-To-Last" }
-    getDescription() { return "Always scroll to last message." }
+    getDescription() { return "Always scroll to last message when entering a channel." }
     getAuthor() { return "square" }
-    getVersion() { return "0.0.2" }
+    getVersion() { return "0.0.3" }
 
-    start(){}
-    stop(){}
-    load(){}
-
-    onSwitch() {
-      var ref;
-      if((ref = document.querySelectorAll(".message-group")) != null && ref.length)
-        ref[ref.length-1].scrollIntoView();
+    start(){
+      initObserver();
     }
+    stop(){
+      switchObserver.disconnect();
+    }
+    load(){}
   }
+
+  onSwitch = function() {
+    ev = new KeyboardEvent("keydown", {bubbles:true});
+    Object.defineProperty(ev, "which", {get:_=>27});
+    document.dispatchEvent(ev);
+  }
+
+  initObserver = function(target) {
+    switchObserver = new MutationObserver(function(mutations) {
+      if(some.call(mutations, function({addedNodes}) {
+        return some.call(addedNodes, function(node) {
+          return node.classList != null && (node.classList.contains("chat") || node.classList.contains("messages-wrapper"));
+        });
+      })) onSwitch();
+    });
+    if((target = document.querySelector("#friends, .chat, .activityFeed-HeiGwL")) != null)
+      switchObserver.observe(target.parentNode, {childList: true, subtree: true});
+  }
+
   return scrollToLast;
 })()
