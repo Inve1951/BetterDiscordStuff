56c77008447a1e038a359a1be31cdb878aeb534e
Inve1951 <Inve1951@jourrapide.com>
2018-01-10 02:27:55 +0100

    default to emojis 0.0.3

--- plugins/defaultToEmojis.plugin.js
+++ plugins/defaultToEmojis.plugin.js
@@ -5,7 +5,7 @@ var defaultToEmojis = (function(){
   class defaultToEmojis {
     getName(){return "Default To Emojis"}
     getDescription(){return "Selects the emoji tab when you open the picker."}
-    getVersion(){return "0.0.2"}
+    getVersion(){return "0.0.3"}
     getAuthor(){return "square"}
 
     start(){}
@@ -13,7 +13,7 @@ var defaultToEmojis = (function(){
     load(){}
 
     observer({addedNodes}){
-      for(i = 0; n = addedNodes[i]; i++) if(n.classList && n.classList.contains("popout") && (n = document.getElementById("bda-qem-emojis"))) {
+      for(i = 0; n = addedNodes[i]; i++) if(n.id === "bda-qem" && (n = document.getElementById("bda-qem-emojis"))) {
         n.click();
         //this.observer = void 0;   // uncomment this to only do it first time
         return;
