8c3e4c205d8ba130b0b6a54d32cbf4bd91bf4c0d
Inve1951 <Inve1951@jourrapide.com>
2018-04-28 11:24:41 +0200

    link profile picture 1.0.2

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -3,7 +3,7 @@
 class linkProfilePicture {
 	getName(){return "Link-Profile-Picture"}
 	getDescription(){return "Lets you click users' avatars on their profile page to view a bigger version in your browser."}
-	getVersion(){return "1.0.1"}
+	getVersion(){return "1.0.2"}
 	getAuthor(){return "square"}
 
 	load(){}
@@ -15,8 +15,8 @@ class linkProfilePicture {
 		var x, i = 0,
 			ref = mutation.addedNodes,
 			wrapper, a, pic, url;
-		while(x = ref[i++]) if("DIV" === x.nodeName && 0 === x.className.indexOf("modal-") && (wrapper = x.querySelector(".avatar-1BXaQj.profile-3z9uol")) &&
-			(pic = wrapper.querySelector(".image-EVRGPw")) && (url = pic.style.backgroundImage.match(/https.+(?:webp|png|gif)/))){
+		while(x = ref[i++]) if("DIV" === x.nodeName && 0 === x.className.indexOf("modal-") && (wrapper = x.querySelector(".avatar-16XVId.profile-ZOdGIb")) &&
+			(pic = wrapper.querySelector(".image-33JSyf")) && (url = pic.style.backgroundImage.match(/https.+(?:webp|png|gif)/))){
 				a = document.createElement("a");
 				a.href = url[0] + "?size=2048";				// returns biggest version (can be smaller than 2048); if the pic is blurry it's due to the original or what discord did to it
 				a.target = "_blank"; a.rel = "noreferrer";
