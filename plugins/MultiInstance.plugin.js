fcc590ad0a3a23b0f7a8b6b4214ec6c8c3aa6ae7
Inve1951 <square3880@gmail.com>
2018-07-17 07:13:25 +0200

    MultiInstance 1.0.1

--- plugins/MultiInstance.plugin.js
+++ plugins/MultiInstance.plugin.js
@@ -1,20 +1,20 @@
 //META { "name": "MultiInstance" } *//
 
 var MultiInstance = function()  {
-  if(MultiInstance !== new.target) throw "You are dumb";
+  let osx = "darwin" === process.platform,
+    listener = ({code, ctrlKey, metaKey}) => code === "F12" && (osx ? metaKey : ctrlKey) && require("child_process").exec(osx
+      ? `open -n -a "${process.argv0.split(".app")[0]}.app" --args --multi-instance`
+      : `"${process.argv0}" --multi-instance`
+    );
 
-  let public = {
+  return {
     getName:_=> "Multi Instance",
-    getDescription:_=> `Have several discord windows open same time. ${process.platform !== "darwin" ? "Ctrl" : "Cmd"}+F12.`,
+    getDescription:_=> `Have several discord windows open same time. ${osx ? "Cmd" : "Ctrl"}+F12.`,
     getAuthor:_=> "square",
-    getVersion:_=> "1.0.0",
+    getVersion:_=> "1.0.1",
     load:_=>_,
 
     start:_=> document.addEventListener("keydown", listener),
     stop:_=> document.removeEventListener("keydown", listener)
-  },
-    listener = ({code, ctrlKey, metaKey}) => code === "F12" && (process.platform !== "darwin" ? ctrlKey : metaKey) && require("child_process").
-      exec(`"${process.argv0}" --multi-instance`);
-
-  for(let name in public) this[name] = public[name];
+  };
 };
