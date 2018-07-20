65f705f3e896b65284627eb3d2b478cddb79e1c4
Inve1951 <square3880@gmail.com>
2018-07-20 18:05:50 +0200

    experiments 1.2.2

--- plugins/discordexperiments.plugin.js
+++ plugins/discordexperiments.plugin.js
@@ -1,29 +1,27 @@
 //META{"name":"discordExperiments"}*//
 
-var discordExperiments = (function(){
-	var t, o = Object, k = "isDeveloper";
+var discordExperiments = function(t){
 	return class _ {
 		getName(){ return "Discord Experiments" }
-		getDescription(){ return "Enables the experiments tab in discord's settings. Depends on samogot's Discord Internals Library: https://git.io/v7Sfp." }
+		getDescription(){ return "Enables the experiments tab in discord's settings." }
 		getAuthor(){ return "square" }
-		getVersion(){ return "1.2.1" }
+		getVersion(){ return "1.2.2" }
 
 		load(){}
 
 		start(){
-			if( "undefined" !== typeof DiscordInternals ) {
-				t = DiscordInternals.WebpackModules.findByUniqueProperties([k]);
-				o.defineProperty(t,k,{get:_=>1,set:_=>_,configurable:true});
-			} else {
-				alert("Look who can't read.\nInstall the Discord Internals Library Plugin or this won't work!\nhttps://git.io/v7Sfp")
-				console.error("Install Discord Internals Library!: https://github.com/samogot/betterdiscord-plugins/blob/master/v1/1lib_discord_internals.plugin.js");
-			}
+			t = BDV2.WebpackModules.findByUniqueProperties(["isDeveloper"]);
+			Object.defineProperty(t,k,{get:_=>1,set:_=>_,configurable:true});
 		}
 
 		stop(){
-			if(t){
-				o.defineProperty(t,k,{get:_=>0,set:_=>{throw new Error("Username is not in the sudoers file. This incident will be reported");},configurable:true});
-			}
+			t && Object.defineProperty(t,"isDeveloper",{
+				get:_=>0,
+				set:_=>{
+					throw new Error("Username is not in the sudoers file. This incident will be reported");
+				},
+				configurable: true
+			});
 		}
 	};
-})();
+}();
