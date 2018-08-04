4d8a49f05d50919abd62e3395591b0913c864201
Inve1951 <square3880@gmail.com>
2018-08-04 15:28:40 +0200

    scroll to last 0.0.4

--- plugins/scrollToLast.plugin.js
+++ plugins/scrollToLast.plugin.js
@@ -1,41 +1,21 @@
 //META { "name": "scrollToLast" } *//
 
-var scrollToLast = (function(some, initObserver, switchObserver, onSwitch, ev){
+var scrollToLast = function() {
+  return {
+    getName: () => "Scroll-To-Last",
+    getDescription: () => "Always scroll to last message when entering a channel.",
+    getAuthor: () => "square",
+    getVersion: () => "0.0.4",
 
-  some = Array.prototype.some;
+    start: ()=>{},
+    stop: ()=>{},
+    load: ()=>{},
 
-  class scrollToLast {
-    getName() { return "Scroll-To-Last" }
-    getDescription() { return "Always scroll to last message when entering a channel." }
-    getAuthor() { return "square" }
-    getVersion() { return "0.0.3" }
-
-    start(){
-      initObserver();
+    onSwitch: ev=> {
+      console.log("switch")
+      ev = new KeyboardEvent("keydown", {bubbles:true});
+      Object.defineProperty(ev, "which", {get:_=>27});
+      document.dispatchEvent(ev);
     }
-    stop(){
-      switchObserver.disconnect();
-    }
-    load(){}
   }
-
-  onSwitch = function() {
-    ev = new KeyboardEvent("keydown", {bubbles:true});
-    Object.defineProperty(ev, "which", {get:_=>27});
-    document.dispatchEvent(ev);
-  }
-
-  initObserver = function(target) {
-    switchObserver = new MutationObserver(function(mutations) {
-      if(some.call(mutations, function({addedNodes}) {
-        return some.call(addedNodes, function(node) {
-          return node.classList != null && (node.classList.contains("chat") || node.classList.contains("messages-wrapper"));
-        });
-      })) onSwitch();
-    });
-    if((target = document.querySelector("#friends, .chat, .activityFeed-HeiGwL")) != null)
-      switchObserver.observe(target.parentNode, {childList: true, subtree: true});
-  }
-
-  return scrollToLast;
-})()
+}
