418ebf617ecc43abbe70162ba1a2aa0e6583711d
Inve1951 <square3880@gmail.com>
2019-01-17 15:30:13 +0100

    enable react devtools v1.0.0

--- plugins/enableReactDevtools.plugin.js
+++ plugins/enableReactDevtools.plugin.js
@@ -1,58 +1,63 @@
-//META { "name": "enableReactDevtools" } *//
+//META { "name": "enableReactDevtools", "website": "https://inve1951.github.io/BetterDiscordStuff/" } *//
 
 var enableReactDevtools = (function(){
-  var listener, bw, versions, path;
+  var listener, path, readdir, _fs, _path, _electron, _remote, _bw;
 
   class enableReactDevtools {
     getName() { return "Enable React-Devtools" }
-    getDescription() { return "Automatically loads the React Devtools for you. Big thanks to Natsulus who helped figgure this out!" }
+    getDescription() { return "Automatically loads the React Devtools for you. Big thanks to Natsulus who helped figure this out!" }
     getAuthor() { return "square" }
-    getVersion() { return "0.0.3" }
+    getVersion() { return "1.0.0" }
 
-    start(){
-      bw = require("electron").remote.BrowserWindow.getAllWindows()[0];
-      bw.webContents.on("devtools-opened", listener);
+    start() {
+      _bw.webContents.on("devtools-opened", listener);
+      if(_bw.webContents.isDevToolsOpened()) listener();
     }
 
-    stop(){
-      bw.webContents.removeListener("devtools-opened", listener);
+    stop() {
+      _bw.webContents.removeListener("devtools-opened", listener);
     }
-
-    load(){}
   }
 
-  try {
-    switch(process.platform){
-    case 'win32':
-      path = require("path").resolve(process.env.LOCALAPPDATA, "Google/Chrome/User Data");
-      break;
-    case 'linux':
-      path = require("path").resolve(process.env.HOME, ".config/google-chrome");
-      break;
-    case 'darwin':
-      path = require("path").resolve(process.env.HOME, "Library/Application Support/Google/Chrome");
-      break;
-    default:
-      console.error("Unsupported platform: " + process.platform);
-    }
-    path += "/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/";
-    versions = require("fs").readdirSync( path );
-    path = require("path").resolve( path, versions[versions.length - 1] )
-  } catch (e) {
-    path = null
-    if (e !== "ENOENT")
-      throw e;
-    console.error("Couldn't find react devtools in chrome extensions!");
-  }
+  _fs = require("fs");
+  _path = require("path");
+  _electron = require("electron");
+  ({remote: _remote} = _electron);
 
-  listener = function(){
-    require("electron").remote.BrowserWindow.removeDevToolsExtension("React Developer Tools");
-    require("electron").webFrame.registerURLSchemeAsSecure("chrome-extension");
-    if( path != null && null != require("electron").remote.BrowserWindow.addDevToolsExtension( path ) )
+  _bw = _remote.getCurrentWindow();
+
+  readdir = require("util").promisify(_fs.readdir);
+
+  _electron.webFrame.registerURLSchemeAsPrivileged("chrome-extension");
+
+  // Should you have react's devtools installed in a different location, you can
+  // override the full path in an `enableReactDevtools.plugin.json` file as `json.path`.
+  path = BdApi.getData("enableReactDevtools", "path") || function(){
+    switch(process.platform) {
+      case 'win32':
+        return _path.resolve(process.env.LOCALAPPDATA, "Google/Chrome/User Data");
+      case 'linux':
+        return _path.resolve(process.env.HOME, ".config/google-chrome");
+      case 'darwin':
+        return _path.resolve(process.env.HOME, "Library/Application Support/Google/Chrome");
+    }
+  }() + "/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/";
+
+  listener = async function() {
+    _remote.BrowserWindow.removeDevToolsExtension("React Developer Tools");
+    var version, bOk = true;
+    try {
+      // Since chrome does the updating, a new version might have been installed and the previous one might no longer be present.
+      [version] = (await readdir(path)).slice(-1);
+    } catch(err) {
+      if("ENOENT" !== err.code) throw err;
+      bOk = false;
+    }
+    if( bOk && _remote.BrowserWindow.addDevToolsExtension(_path.join(path, version)) )
       console.log("Successfully installed react devtools.");
     else
       console.error("Couldn't find react devtools in chrome extensions!");
   }
 
   return enableReactDevtools;
-})()
+})();
