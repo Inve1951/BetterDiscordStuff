5b2b7f140ab1dd29942a80fe0a28053e883f7b9d
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-07-22 17:35:44 -0700

    Update linkProfilePicture.plugin.js

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,28 +1,33 @@
-//META{"name":"linkProfilePicture"}*//
+//META{"name":"linkProfilePicture","source":"https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/linkProfilePicture.plugin.js","website":"https://github.com/Inve1951"}*//
 
 class linkProfilePicture {
 	getName(){return "Link-Profile-Picture"}
 	getDescription(){return "Lets you click users' avatars on their profile page to view a bigger version in your browser."}
-	getVersion(){return "1.0.3"}
+	getVersion(){return "1.0.4"}
 	getAuthor(){return "square"}
 
 	load(){}
-
 	start(){}
 	stop(){}
 
-	observer(mutation){
-		var x, i = 0,
-			ref = mutation.addedNodes,
-			wrapper, a, pic, url;
-		while(x = ref[i++]) if("DIV" === x.nodeName && 0 === x.className.indexOf("modal-") && (wrapper = x.querySelector(".avatar-3EQepX.profile-ZOdGIb")) &&
-			(pic = wrapper.querySelector(".image-33JSyf")) && (url = pic.style.backgroundImage.match(/https.+(?:webp|png|gif)/))){
-				a = document.createElement("a");
-				a.href = url[0] + "?size=2048";				// returns biggest version (can be smaller than 2048); if the pic is blurry it's due to the original or what discord did to it
-				a.target = "_blank"; a.rel = "noreferrer";
-				wrapper.insertBefore(a, pic);
-				a.appendChild(pic);
-			break;
+	observer({addedNodes}){
+		if(addedNodes[0]&&addedNodes[0].hasClass&&addedNodes[0].hasClass('modal-3c3bKg')&&addedNodes[0].getElementsByClassName('avatar-VxgULZ')[0]){
+			var a,parent=addedNodes[0].getElementsByClassName('avatar-3EQepX')[0],child=parent.getElementsByTagName('svg')[0],img=child.getElementsByClassName('avatar-VxgULZ')[0];
+			if(parent&&child){
+				a=this.parseHTML(`<a class="linkProfilePicture" target="_blank" rel="noreferrer" href="${img.src.match(/https.+(?:webp|png|gif)/)[0]}?size=2048">
+					${child.outerHTML}
+				</a>`)[0];
+				parent.replaceChild(a,child);
+			}
 		}
+	}
+
+	parseHTML(html=[]){
+		if(html.constructor===String&&/(<[^<>]+>)/g.test(html)){
+			var template=document.createElement('template');
+			template.innerHTML=html.trim();
+			return template.content.childNodes;
+		}
+		return null;
 	}
 }
