3a9244e56e303bfc9cf885e7634252eec3aadf00
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-07-25 18:39:11 -0700

    Update linkProfilePicture.plugin.js

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -11,8 +11,8 @@ class linkProfilePicture {
 	stop(){}
 
 	observer({addedNodes}){
-		//AddedNodes only ever returns one node or nothing no need to iterate over it. Can return text nodes and the Notification settings panel shares the same class, so check that classList contains the correct class and a certain child element.
-		if(addedNodes[0]&&addedNodes[0] instanceof Element&&addedNodes[0].classList.contains('modal-3c3bKg')&&addedNodes[0].getElementsByClassName('avatar-VxgULZ')[0]){
+		//AddedNodes only ever returns one node or nothing no need to iterate over it. Check to see if the first index of the array has something, nodeType to make sure it is an element, make sure it is the correct modal by both it's classname and the avatar descendant.
+		if(addedNodes[0]&&addedNodes[0].nodeType===Node.ELEMENT_NODE&&addedNodes[0].classList.contains('modal-3c3bKg')&&addedNodes[0].getElementsByClassName('avatar-VxgULZ')[0]){
 			var a,parent=addedNodes[0].getElementsByClassName('avatar-3EQepX')[0],child=parent.getElementsByTagName('svg')[0],img=child.getElementsByClassName('avatar-VxgULZ')[0];
 			if(!parent||!child||!img)return;//Check if something went wrong.
 			a=this.parseHTML(`<a class="linkProfilePicture" target="_blank" rel="noreferrer" href="${img.src.match(/https.+(?:webp|png|gif)/)[0]}?size=2048"></a>`)[0];
