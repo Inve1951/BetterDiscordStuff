f9555b2a00e05edeaa49d55c08ea496804f66404
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-07-29 23:41:11 -0700

    Don't get eles in relation to other eles that can be undefined like a 4head

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -13,7 +13,7 @@ class linkProfilePicture {
 	observer({addedNodes}){
 		//AddedNodes only ever returns one node or nothing no need to iterate over it. Check to see if the first index of the array has something, nodeType to make sure it is an element, make sure it is the correct modal by both it's classname and the avatar descendant.
 		if(addedNodes[0]&&addedNodes[0].nodeType===Node.ELEMENT_NODE&&addedNodes[0].classList.contains('modal-3c3bKg')&&addedNodes[0].getElementsByClassName('avatar-VxgULZ')[0]){
-			var a,parent=addedNodes[0].getElementsByClassName('avatar-3EQepX')[0],child=parent.getElementsByTagName('svg')[0],img=child.getElementsByClassName('avatar-VxgULZ')[0];
+			var a,parent=addedNodes[0].getElementsByClassName('avatar-3EQepX')[0],child=addedNodes[0].getElementsByClassName('mask-1l8v16')[0],img=addedNodes[0].getElementsByClassName('avatar-VxgULZ')[0];
 			if(!parent||!child||!img)return;//Check if something went wrong.
 			a=this.parseHTML(`<a class="linkProfilePicture" target="_blank" rel="noreferrer" href="${img.src.match(/https.+(?:webp|png|gif)/)[0]}?size=2048"></a>`)[0];
 			a.appendChild(child);//Move the element named "child" to be a child of our "a" element.
