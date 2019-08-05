21e949daf461b823860acc027a4d8e8007d38315
Inve1951 <square3880@gmail.com>
2019-08-05 20:22:31 +0200

    MultiInstance v1.0.2 - fixes #41

--- plugins/MultiInstance.plugin.js
+++ plugins/MultiInstance.plugin.js
@@ -4,14 +4,14 @@ var MultiInstance = function()  {
   let osx = "darwin" === process.platform,
     listener = ({code, ctrlKey, metaKey}) => code === "F12" && (osx ? metaKey : ctrlKey) && require("child_process").exec(osx
       ? `open -n -a "${process.argv0.split(".app")[0]}.app" --args --multi-instance`
-      : `"${process.argv0}" --multi-instance`
+      : `"${process.execPath}" --multi-instance`
     );
 
   return {
     getName:_=> "Multi Instance",
     getDescription:_=> `Have several discord windows open same time. ${osx ? "Cmd" : "Ctrl"}+F12.`,
     getAuthor:_=> "square",
-    getVersion:_=> "1.0.1",
+    getVersion:_=> "1.0.2",
     load:_=>_,
 
     start:_=> document.addEventListener("keydown", listener),
