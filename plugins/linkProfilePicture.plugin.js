798fe2ce8a59aab8c1b2ac88cd7b688abc3ee0c4
square <Inve1951@users.noreply.github.com>
2019-11-23 11:08:27 +0000

    fix whitespace

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,28 +1,28 @@
 //META{"name":"linkProfilePicture","source":"https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/linkProfilePicture.plugin.js","website":"https://Inve1951.github.io/BetterDiscordStuff"}*//
 
 class linkProfilePicture {
-	getName(){return "Link-Profile-Picture"}
-	getDescription(){return "Lets you click users' avatars on their profile page to view a bigger version in your browser."}
-	getVersion(){return "1.0.4"}
-	getAuthor(){return "square"}
+  getName(){return "Link-Profile-Picture"}
+  getDescription(){return "Lets you click users' avatars on their profile page to view a bigger version in your browser."}
+  getVersion(){return "1.0.4"}
+  getAuthor(){return "square"}
 
-	start(){}
-	stop(){}
+  start(){}
+  stop(){}
 
-	observer({addedNodes: [modal]}){
+  observer({addedNodes: [modal]}){
     var wrapper, img, a;
-		if(
+    if(
       modal && Node.ELEMENT_NODE === modal.nodeType && modal.classList.contains("modal-3c3bKg") &&
       (wrapper = modal.querySelector(".avatar-3EQepX")) &&
       (img = wrapper.querySelector(".avatar-VxgULZ"))
     ){
-			a = document.createElement("a");
+      a = document.createElement("a");
       a.id = "LinkProfilePicture"
-			a.href = img.src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"); // returns biggest version (can be smaller than 4096); if the pic is blurry it's due to the original or what discord did to it
-			a.target = "_blank";
-			a.rel = "noreferrer";
+      a.href = img.src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"); // returns biggest version (can be smaller than 4096); if the pic is blurry it's due to the original or what discord did to it
+      a.target = "_blank";
+      a.rel = "noreferrer";
       wrapper.insertBefore(a, wrapper.firstChild);
-			a.appendChild(a.nextSibling);
-		}
-	}
+      a.appendChild(a.nextSibling);
+    }
+  }
 }
