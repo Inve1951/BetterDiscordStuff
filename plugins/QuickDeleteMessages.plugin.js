478d9961f9fa4e15ee7e70e66c92a885005b9f55
Master3395 <master3395@users.noreply.github.com>
2020-09-17 17:55:01 +0200

    QDM v1.4.2
    Thank you @master3395, @Glaived, @Nox-Git and sorry for the late response.
    fixes #84

--- plugins/QuickDeleteMessages.plugin.js
+++ plugins/QuickDeleteMessages.plugin.js
@@ -1,117 +1,117 @@
 //META { "name": "QuickDeleteMessages", "website": "https://inve1951.github.io/BetterDiscordStuff/" } *//
 global.QuickDeleteMessages = function () {
-  var AsyncKeystate, EndpointMessages, MessagePrompts, Permissions, UserStore, _qualifies, getOwnerInstance, gotDeletePermission, onClick, settings;
+var AsyncKeystate, EndpointMessages, MessagePrompts, Permissions, UserStore, _qualifies, getOwnerInstance, gotDeletePermission, onClick, settings;
 
-  class QuickDeleteMessages {
-    getName() {
-      return "Quick Delete Messages";
-    }
+class QuickDeleteMessages {
+  getName() {
+    return "Quick Delete Messages";
+  }
 
-    getDescription() {
-      return "Hold Delete and click a Message to delete it.";
-    }
+  getDescription() {
+    return "Hold Delete and click a message to delete it.";
+  }
 
-    getAuthor() {
-      return "square";
-    }
+  getAuthor() {
+    return "square";
+  }
 
-    getVersion() {
-      return "1.4.1";
-    }
+  getVersion() {
+    return "1.4.2";
+  }
 
-    load() {
-      return window.SuperSecretSquareStuff != null ? window.SuperSecretSquareStuff : window.SuperSecretSquareStuff = new Promise(function (c, r) {
-        return require("request").get("https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/0circle.plugin.js", function (err, res, body) {
-          if (err || 200 !== (res != null ? res.statusCode : void 0)) {
-            return r(err != null ? err : res);
+  load() {
+    return window.SuperSecretSquareStuff != null ? window.SuperSecretSquareStuff : window.SuperSecretSquareStuff = new Promise(function (c, r) {
+      return require("request").get("https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/0circle.plugin.js", function (err, res, body) {
+        if (err || 200 !== (res != null ? res.statusCode : void 0)) {
+          return r(err != null ? err : res);
+        }
+        Object.defineProperties(window.SuperSecretSquareStuff, {
+          libLoaded: {
+            value: c
+          },
+          code: {
+            value: body
           }
-          Object.defineProperties(window.SuperSecretSquareStuff, {
-            libLoaded: {
-              value: c
-            },
-            code: {
-              value: body
-            }
-          });
-          return (0, eval)(body);
         });
+        return (0, eval)(body);
       });
+    });
+  }
+
+  async start() {
+    var ref;
+    ({ AsyncKeystate, getOwnerInstance } = await SuperSecretSquareStuff);
+    settings.confirm = (ref = BdApi.getData("QuickDeleteMessages", "confirm")) != null ? ref : false;
+    if (UserStore == null) {
+      UserStore = BdApi.findModuleByProps("getCurrentUser");
     }
-
-    async start() {
-      var ref;
-      ({ AsyncKeystate, getOwnerInstance } = await SuperSecretSquareStuff);
-      settings.confirm = (ref = bdPluginStorage.get("QuickDeleteMessages", "confirm")) != null ? ref : false;
-      if (UserStore == null) {
-        UserStore = BdApi.findModuleByProps("getCurrentUser");
-      }
-      if (Permissions == null) {
-        Permissions = BdApi.findModuleByProps("computePermissions");
-      }
-      if (EndpointMessages == null) {
-        EndpointMessages = BdApi.findModuleByProps("deleteMessage");
-      }
-      if (MessagePrompts == null) {
-        MessagePrompts = BdApi.findModuleByProps("confirmDelete");
-      }
-      return document.addEventListener("click", onClick, true);
+    if (Permissions == null) {
+      Permissions = BdApi.findModuleByProps("computePermissions");
     }
-
-    stop() {
-      return document.removeEventListener("click", onClick, true);
+    if (EndpointMessages == null) {
+      EndpointMessages = BdApi.findModuleByProps("deleteMessage");
     }
-
-    getSettingsPanel() {
-      return `<label style="color: #87909C"><input type="checkbox" name="confirm" onChange="QuickDeleteMessages.updateSettings(this)"\n${settings.confirm && "checked" || ""} />confirm delete?</label>`;
+    if (MessagePrompts == null) {
+      MessagePrompts = BdApi.findModuleByProps("confirmDelete");
     }
+    return document.addEventListener("click", onClick, true);
+  }
 
-    static updateSettings({ name, checked }) {
-      settings[name] = checked;
-      BdApi.saveData("QuickDeleteMessages", name, checked);
-    }
+  stop() {
+    return document.removeEventListener("click", onClick, true);
+  }
 
-  };
+  getSettingsPanel() {
+    return `<label style="color: #87909C"><input type="checkbox" name="confirm" onChange="QuickDeleteMessages.updateSettings(this)"\n${settings.confirm && "checked" || ""} />confirm delete?</label>`;
+  }
 
-  settings = Object.create(null);
+  static updateSettings({ name, checked }) {
+    settings[name] = checked;
+    BdApi.saveData("QuickDeleteMessages", name, checked);
+  }
 
-  Permissions = UserStore = EndpointMessages = MessagePrompts = null;
+};
 
-  AsyncKeystate = getOwnerInstance = null;
+settings = Object.create(null);
 
-  _qualifies = ".contentCozy-3XX413, .messageCompact-kQa7ES";
+Permissions = UserStore = EndpointMessages = MessagePrompts = null;
 
-  onClick = function (event) {
-    var channel, element, message, shiftKey;
-    if (!(AsyncKeystate.key("Delete") || "darwin" === process.platform && AsyncKeystate.key("Backspace"))) {
-      return;
-    }
-    ({
-      path: [element],
-      shiftKey
-    } = event);
-    if (!(element.matches(_qualifies) || (element = element.closest(_qualifies)))) {
-      return;
-    }
-    ({
-      props: { channel, message }
-    } = getOwnerInstance(element));
-    if (!gotDeletePermission(channel, message)) {
-      return;
-    }
-    if (settings.confirm && !shiftKey) {
-      MessagePrompts.confirmDelete(channel, message, false);
-    } else {
-      EndpointMessages.deleteMessage(channel.id, message.id, false);
-    }
-    event.preventDefault();
-    event.stopImmediatePropagation();
-  };
+AsyncKeystate = getOwnerInstance = null;
 
-  gotDeletePermission = function (channel, message) {
-    var self;
-    self = UserStore.getCurrentUser();
-    return self === message.author || 0x2000 & Permissions.computePermissions(self, channel);
-  };
+_qualifies = ".message-2qnXI6";
 
-  return QuickDeleteMessages;
-}.call(this);
+onClick = function (event) {
+  var channel, element, message, shiftKey;
+  if (!(AsyncKeystate.key("Delete") || "darwin" === process.platform && AsyncKeystate.key("Backspace"))) {
+    return;
+  }
+  ({
+    path: [element],
+    shiftKey
+  } = event);
+  if (!(element.matches(_qualifies) || (element = element.closest(_qualifies)))) {
+    return;
+  }
+  ({
+    props: { channel, message }
+  } = SuperSecretSquareStuff.ReactDom._internal.ReactDOMComponentTree.getClosestInstanceFromNode(element).return.pendingProps.childrenAccessories);
+  if (!gotDeletePermission(channel, message)) {
+    return;
+  }
+  if (settings.confirm && !shiftKey) {
+    MessagePrompts.confirmDelete(channel, message, false);
+  } else {
+    EndpointMessages.deleteMessage(channel.id, message.id, false);
+  }
+  event.preventDefault();
+  event.stopImmediatePropagation();
+};
+
+gotDeletePermission = function (channel, message) {
+  var self;
+  self = UserStore.getCurrentUser();
+  return self === message.author || 0x2000 & Permissions.computePermissions(self, channel);
+};
+
+return QuickDeleteMessages;
+}.call(this);  
