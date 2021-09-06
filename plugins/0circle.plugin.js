04ee5c0f5ea8c82b3479f6a535a39f4d19d7a4ab
square <square3880@gmail.com>
2021-09-06 17:02:16 +0200

    more tools for the lib

--- plugins/0circle.plugin.js
+++ plugins/0circle.plugin.js
@@ -22,14 +22,17 @@ SquareLib = function () {
 };
 
 (async function () {
-  var AsyncKeystate, React, ReactDOM, _crypto, _fs, _keystates, _path, createElement, createReadOnlyObject, env, err, fs, generateMd5, getOwnerInstance, promise, promisify, filename, dirname;
+  var AsyncKeystate, React, ReactDOM, _crypto, _fs, _keystates, _path, createElement, createReadOnlyObject, dirname, env, err, filename, findDescendant, fs, generateMd5, getOwnerInstance, patchRender, promise, promisify;
+
   _path = require("path");
   _fs = require("fs");
   _crypto = require("crypto");
-  filename = typeof __filename !== "undefined" && __filename || "";
-  dirname = typeof __dirname !== "undefined" && __dirname || "";
+  filename = typeof __filename !== "undefined" && __filename !== null ? __filename : "";
+  dirname = typeof __dirname !== "undefined" && __dirname !== null ? __dirname : "";
+
   createReadOnlyObject = function (obj) {
     var k, v;
+
     for (k in obj) {
       if (!hasProp.call(obj, k)) continue;
       v = obj[k];
@@ -38,11 +41,14 @@ SquareLib = function () {
         value: v
       };
     }
+
     return Object.defineProperties(Object.create(null), obj);
   };
+
   env = createReadOnlyObject({
     INSTALLED: filename.endsWith(".plugin.js")
   });
+
   promisify = function (f, _this = null) {
     return function (...args) {
       return new Promise(function (c, r) {
@@ -50,43 +56,60 @@ SquareLib = function () {
           if (err) {
             return r(err);
           }
+
           return c(res);
         }));
       });
     };
   };
+
   fs = createReadOnlyObject(function () {
     var i, k, len, o, ref;
     o = {};
     ref = ["rename"];
+
     for (i = 0, len = ref.length; i < len; i++) {
       k = ref[i];
       o[k] = promisify(_fs[k], _fs);
     }
+
     return o;
   }());
+
   generateMd5 = function (str) {
     return _crypto.createHash("md5").update(str).digest("hex");
   };
+
   getOwnerInstance = function (elem, options = {}, filter = function () {
     return true;
   }) {
     var ignoreHtml, ignoreSymbols, returnStateNode, stateNodeIsMandatory;
+
     if ("function" === typeof options) {
       filter = options;
       options = {};
     }
-    ({ stateNodeIsMandatory = true, ignoreHtml = true, ignoreSymbols = true, returnStateNode = true } = options);
+
+    ({
+      stateNodeIsMandatory = true,
+      ignoreHtml = true,
+      ignoreSymbols = true,
+      returnStateNode = true
+    } = options);
+
     while (elem) {
       if (elem instanceof Node) {
         elem = ReactDOM._internal.ReactDOMComponentTree.getClosestInstanceFromNode(elem);
       }
+
       if (!(elem = elem.return)) {
         break;
       }
+
       if (null === elem.type || stateNodeIsMandatory && !elem.stateNode || ignoreHtml && elem.stateNode instanceof Node || ignoreSymbols && "symbol" === typeof elem.type) {
         continue;
       }
+
       if (filter(elem)) {
         if (returnStateNode) {
           return elem.stateNode;
@@ -95,8 +118,10 @@ SquareLib = function () {
         }
       }
     }
+
     return null;
   };
+
   _keystates = [];
   AsyncKeystate = new Proxy(Object.freeze(Object.create(null)), {
     get: function (_, type) {
@@ -108,9 +133,13 @@ SquareLib = function () {
     }
   });
   document.addEventListener("keydown", function (ev) {
-    return _keystates.push(ev);
+    if (!ev.repeat) {
+      return _keystates.push(ev);
+    }
   }, true);
-  document.addEventListener("keyup", function ({ code }) {
+  document.addEventListener("keyup", function ({
+    code
+  }) {
     return _keystates = _keystates.filter(function (ev) {
       return ev.code !== code;
     });
@@ -118,9 +147,11 @@ SquareLib = function () {
   window.addEventListener("blur", function () {
     return _keystates = [];
   }, false);
+
   createElement = function (tag, props = {}, ...children) {
     var c, elem, i, k, len, v;
     elem = document.createElement(tag);
+
     if (props) {
       for (k in props) {
         if (!hasProp.call(props, k)) continue;
@@ -128,18 +159,230 @@ SquareLib = function () {
         elem[k] = v;
       }
     }
+
     for (i = 0, len = children.length; i < len; i++) {
       c = children[i];
       elem.appendChild(c);
     }
+
     return elem;
   };
+
+  findDescendant = function (node, options = {}, filter = function () {
+    return true;
+  }, parent) {
+    var child, children, i, ignoreHtml, ignoreSymbols, j, len, len1, returnStateNode, stateNodeIsMandatory;
+
+    if ("function" === typeof options) {
+      filter = options;
+      options = {};
+    }
+
+    ({
+      stateNodeIsMandatory = true,
+      ignoreHtml = true,
+      ignoreSymbols = true,
+      returnStateNode = true
+    } = options);
+
+    if (node instanceof Element) {
+      // TODO: implement (virtual) DOM version
+      return null;
+    }
+
+    if (node instanceof Array) {
+      for (i = 0, len = node.length; i < len; i++) {
+        child = node[i];
+
+        if (child && (child = findDescendant(child, options, filter, node))) {
+          return child;
+        }
+      }
+    }
+
+    if (!React.isValidElement(node)) {
+      return null;
+    }
+
+    ({
+      props: {
+        children
+      }
+    } = node);
+
+    if (!children) {
+      return null;
+    }
+
+    if (!(children instanceof Array)) {
+      children = [children];
+    }
+
+    for (j = 0, len1 = children.length; j < len1; j++) {
+      child = children[j];
+
+      if (!child) {
+        continue;
+      }
+
+      if (!(null === child.type || stateNodeIsMandatory && !child.stateNode || ignoreHtml && child.stateNode instanceof Node || ignoreSymbols && "symbol" === typeof child.type) && filter(child)) {
+        if (returnStateNode) {
+          return child.stateNode;
+        } else {
+          return [child, node];
+        }
+      }
+
+      if (child = findDescendant(child, options, filter, node)) {
+        return child;
+      }
+    }
+
+    return null;
+  };
+
+  patchRender = function (component, ...patches) {
+    var componentBase, originalRender, renderKey;
+
+    if ("string" === typeof patches[0]) {
+      renderKey = patches.shift();
+    }
+
+    if (component.prototype instanceof React.Component) {
+      componentBase = component.prototype;
+      renderKey || (renderKey = "render");
+    } else {
+      componentBase = component;
+      renderKey || (renderKey = "default");
+    }
+
+    if ("function" !== typeof componentBase[renderKey]) {
+      throw new Error("Unsupported component model");
+    }
+
+    originalRender = componentBase[renderKey];
+
+    componentBase[renderKey] = function () {
+      var _filter, children, content, err, filter, i, len, mode, node, parent, props, render, res, state, target, touch;
+
+      res = Reflect.apply(originalRender, this, arguments);
+      [props, state] = arguments;
+
+      if (this.props) {
+        ({
+          props
+        } = this);
+      }
+
+      if (this.state) {
+        ({
+          state
+        } = this);
+      }
+
+      for (i = 0, len = patches.length; i < len; i++) {
+        ({
+          filter = function () {
+            return true;
+          },
+          mode,
+          render,
+          touch
+        } = patches[i]);
+
+        _filter = function (node) {
+          return node && filter(node, props, state);
+        };
+
+        try {
+          if (!(target = mode !== "before" && mode !== "after" && _filter(res) ? [res] : findDescendant(res, {
+            returnStateNode: false,
+            ignoreHtml: false,
+            stateNodeIsMandatory: false
+          }, _filter))) {
+            continue;
+          }
+
+          [node, parent] = target;
+          content = "function" === typeof render ? Reflect.apply(render, this, arguments) : render;
+
+          if (mode === "append" || mode === "prepend") {
+            target = node;
+            mode = "append" === mode ? "after" : "before";
+          } else if (mode === "before" || mode === "after") {
+            target = parent;
+          } else if (render) {
+            mode = "replace";
+          }
+
+          if ("function" === typeof touch) {
+            Reflect.apply(touch, this, [node, props, state]);
+          }
+
+          if ("replace" === mode) {
+            if (!parent) {
+              return content;
+            }
+
+            if (parent instanceof Array) {
+              parent[parent.indexOf(node)] = content;
+            } else {
+              ({
+                children
+              } = parent.props);
+
+              if (children instanceof Array) {
+                children[children.indexOf(node)] = content;
+              } else {
+                parent.props.children = content;
+              }
+            }
+          } else if (mode) {
+            if (!(target instanceof Array)) {
+              ({
+                children
+              } = target.props);
+
+              if (children instanceof Array) {
+                target = children;
+              } else {
+                ({
+                  props: {
+                    children: target
+                  }
+                } = target.props.children = React.createElement(React.Fragment, null, React.Children.toArray(children)));
+              }
+            }
+
+            target["before" === mode ? "unshift" : "push"](content);
+          }
+        } catch (error) {
+          err = error;
+          console.error(err);
+        }
+      }
+
+      return res;
+    };
+
+    return function () {
+      // TODO: forceUpdate()
+      return componentBase[renderKey] = originalRender;
+    };
+  };
+
   try {
-    ({ React, ReactDOM } = BdApi);
-    ReactDOM = Object.assign({}, ReactDOM, {
+    ({
+      React,
+      ReactDOM
+    } = BdApi);
+    ReactDOM = createReadOnlyObject({ ...ReactDOM,
       _internal: function () {
         var Events, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9;
-        ({ Events } = ReactDOM.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+
+        ({
+          Events
+        } = ReactDOM.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
         [_0, _1, _2, _3, _4, _5, _6, _7, _8, _9] = Events;
         return createReadOnlyObject({
           Events,
@@ -147,10 +390,12 @@ SquareLib = function () {
             getInstanceFromNode: _0,
             getClosestInstanceFromNode: function (node) {
               var instance;
+
               while (!(instance || !node)) {
                 instance = _0(node);
                 node = node.parentNode;
               }
+
               return instance;
             },
             getNodeFromInstance: _1,
@@ -180,20 +425,30 @@ SquareLib = function () {
     err = error;
     console.error(err);
   }
+
   promise = window.SuperSecretSquareStuff;
+
   if (env.INSTALLED) {
     try {
       if ("219363409097916416" !== BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id) {
-        alert("SquareLib is not supposed to be installed locally.\nPlugins which use it will load it in remotely.\nHaving an outdated version of this lib installed tends to cause plugin issues and breakage.\nFeel free to keep it anyway and enjoy this popup as a reminder.");
+        alert(`SquareLib is not supposed to be installed locally.
+Plugins which use it will load it in remotely.
+Having an outdated version of this lib installed tends to cause plugin issues and breakage.
+Feel free to keep it anyway and enjoy this popup as a reminder.`);
       }
     } catch (error) {}
+
     if ("0circle.plugin.js" !== _path.basename(filename)) {
       return await fs.rename(filename, _path.join(dirname, "0circle.plugin.js"));
     }
   }
+
   if (promise && !(promise instanceof Promise && "function" === typeof promise.libLoaded && "string" === typeof promise.code)) {
-    return alert("Couldn't load SquareLib.\nIf you are a plugin author, you likely messed up.\nIf you are not, and just installed or updated a plugin, it's author likely messed up.");
+    return alert(`Couldn't load SquareLib.
+If you are a plugin author, you likely messed up.
+If you are not, and just installed or updated a plugin, it's author likely messed up.`);
   }
+
   Object.defineProperty(window, "SuperSecretSquareStuff", {
     value: createReadOnlyObject({
       _hash: promise ? generateMd5(promise.code) : null,
@@ -206,20 +461,21 @@ SquareLib = function () {
       generateMd5,
       AsyncKeystate,
       React,
-      ReactDOM
+      ReactDOM,
+      findDescendant,
+      patchRender
     })
   });
   return promise && promise.libLoaded(window.SuperSecretSquareStuff);
 })();
-
 /* Loader:
-load: ->
-  window.SuperSecretSquareStuff ?= new Promise (c, r) ->
-    require("request").get "https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/0circle.plugin.js", (err, res, body) ->
-      return r err ? res if err or 200 isnt res?.statusCode
-      Object.defineProperties window.SuperSecretSquareStuff, {libLoaded: value: c; code: value: body}
-      `(0,eval)(body)`
+  load: ->
+    window.SuperSecretSquareStuff ?= new Promise (c, r) ->
+      require("request").get "https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/0circle.plugin.js", (err, res, body) ->
+        return r err ? res if err or 200 isnt res?.statusCode
+        Object.defineProperties window.SuperSecretSquareStuff, {libLoaded: value: c; code: value: body}
+        `(0,eval)(body)`
 
-start: ->
-  await SuperSecretSquareStuff
+  start: ->
+    await SuperSecretSquareStuff
 */
