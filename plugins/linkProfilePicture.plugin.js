e74161e502c698ad623cd59964c87c8b5c9a717f
pipipear <65990558+pipipear@users.noreply.github.com>
2022-01-09 14:17:33 -0800

    Reorganised functions and added CSS
    CSS: a "view in browser" message on avatar hover in the profile pop-out

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Link-Profile-Picture
  * @description Lets you click users' avatars on their profile page to view a bigger version in your browser.
- * @version 1.2.1
+ * @version 1.2.2
  * @author square
  * @authorLink https://betterdiscord.app/developer/square
  * @website https://betterdiscord.app/plugin/Link-Profile-Picture
@@ -10,13 +10,47 @@
  */
 
 module.exports = class linkProfilePicture {
-  start() {
-    document.addEventListener("click", LinkProfilePicture, true);
-    this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
-    function LinkProfilePicture({ target }) {
-      if (target.classList.contains("avatar-AvHqJA") && target.parentElement.classList.contains("header-4zuFdR")) {
-        window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
-      }
+  LinkProfilePicture({ target }) {
+    if (target.classList.contains("avatar-AvHqJA") && target.parentElement.classList.contains("header-4zuFdR")) {
+      window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
     }
+  }
+
+  start() {
+    document.addEventListener("click", this.LinkProfilePicture, true);
+    BdApi.injectCSS('linkProfilePicture', `
+      .header-4zuFdR > .avatar-AvHqJA svg > foreignObject > div::after {
+        position: absolute;
+        content: 'view in browser';
+        box-sizing: border-box;
+        font-size: 10px;
+        line-height: 12px;
+        font-weight: 700;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        text-transform: uppercase;
+        text-shadow: 2px 2px 20px #000000, 2px 2px 20px #000000;
+        color: #fff;
+        top: 0;
+        left: 0;
+        transition: opacity .1s ease;
+        opacity: 0;
+        pointer-events: none;
+      }
+      .header-4zuFdR > .avatar-AvHqJA:hover svg > foreignObject > div::after {
+        opacity: 1;
+      }
+    `);
+  }
+
+  stop() {
+    document.removeEventListener("click", this.LinkProfilePicture, true);
+    BdApi.clearCSS('linkProfilePicture');
   }
 };
