ca2aeb563b318fe12bbc0cae15f7123b214b2798
Duy Pham Le <phamleduy04@gmail.com>
2022-01-13 18:15:18 -0600

    Update linkProfilePicture.plugin.js

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Link-Profile-Picture
  * @description Lets you click users' avatars on their profile page to view a bigger version in your browser.
- * @version 1.2.1
+ * @version 1.2.2
  * @author square
  * @authorLink https://betterdiscord.app/developer/square
  * @website https://betterdiscord.app/plugin/Link-Profile-Picture
@@ -9,12 +9,12 @@
  * @updateUrl https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/linkProfilePicture.plugin.js
  */
 
-module.exports = class linkProfilePicture {
+ module.exports = class linkProfilePicture {
   start() {
     document.addEventListener("click", LinkProfilePicture, true);
     this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
     function LinkProfilePicture({ target }) {
-      if (target.classList.contains("avatar-AvHqJA") && target.parentElement.classList.contains("header-4zuFdR")) {
+      if (target.classList.contains("avatar-3QF_VA") && target.parentElement.classList.contains("header-S26rhB")) {
         window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
       }
     }
