3a782e05c5380ba1afa61756d0c417ca3748a121
square <square3880@gmail.com>
2022-08-02 01:21:22 +0200

    Discord Experiments v1.3.0

--- plugins/discordexperiments.plugin.js
+++ plugins/discordexperiments.plugin.js
@@ -1,9 +1,11 @@
 /**
  * @name discordExperiments
- * @website https://betterdiscord.app
  * @description Enables the experiments tab in discord's settings.
  * @author square
  * @version 1.3.0
+ * @website https://betterdiscord.app/plugin/Discord%20Experiments
+ * @source https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/discordexperiments.plugin.js
+ * @updateUrl https://betterdiscord.app/gh-redirect?id=206
  */
 
 const storeExports = BdApi.findModule(m => Reflect.has(m?.default, "isDeveloper"));
