ea34c3aa3405e030ddeda790de2cc5c34f6a2a28
AAGaming <aagaming00@protonmail.com>
2022-08-01 22:27:10 -0400

    Discord Experiments v1.3.1

--- plugins/discordexperiments.plugin.js
+++ plugins/discordexperiments.plugin.js
@@ -2,27 +2,27 @@
  * @name discordExperiments
  * @description Enables the experiments tab in discord's settings.
  * @author square
- * @version 1.3.0
+ * @version 1.3.1
  * @website https://betterdiscord.app/plugin/Discord%20Experiments
  * @source https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/discordexperiments.plugin.js
  * @updateUrl https://betterdiscord.app/gh-redirect?id=206
  */
 
-const storeExports = BdApi.findModule(m => Reflect.has(m?.default, "isDeveloper"));
-const original = storeExports.default;
+const settingsExports = BdApi.findModule(m => m?.default?.toString().includes("Developer Options"));
+const original = settingsExports.default;
 
 module.exports = class {
 	getName(){ return "Discord Experiments"; }
 
 	start() {
-    storeExports.default = new Proxy(original, {
-			get(_, key) {
-				return key === "isDeveloper" ? true : original[key];
-			}
-		});
+		settingsExports.default = (...args) => {
+			let ret = original(...args)
+			ret.forEach(e => e.predicate?.toString()?.includes("isDeveloper") && delete e.predicate)
+			return ret;
+		}
 	}
 
 	stop(){
-		storeExports.default = original;
+		settingsExports.default = original;
 	}
 };
