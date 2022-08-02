9281d36d05ca25cd33aec9027fc7e9745f887c00
AAGaming <aa@mail.catvibers.me>
2022-08-01 23:33:00 -0400

    better

--- plugins/discordexperiments.plugin.js
+++ plugins/discordexperiments.plugin.js
@@ -1,28 +1,30 @@
 /**
  * @name discordExperiments
  * @description Enables the experiments tab in discord's settings.
- * @author square
+ * @author square, AAGaming00
  * @version 1.3.1
  * @website https://betterdiscord.app/plugin/Discord%20Experiments
  * @source https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/discordexperiments.plugin.js
  * @updateUrl https://betterdiscord.app/gh-redirect?id=206
  */
 
-const settingsExports = BdApi.findModule(m => m?.default?.toString().includes("Developer Options"));
-const original = settingsExports.default;
+const settingsStore = BdApi.findModule(m => typeof m?.default?.isDeveloper !== "undefined");
+const userStore = BdApi.findModule(m => m?.exports?.default?.getUsers);
 
 module.exports = class {
 	getName(){ return "Discord Experiments"; }
 
 	start() {
-		settingsExports.default = (...args) => {
-			let ret = original(...args)
-			ret.forEach(e => e.predicate?.toString()?.includes("isDeveloper") && delete e.predicate)
-			return ret;
-		}
+		try {
+			settingsStore.default._dispatcher._orderedActionHandlers.CONNECTION_OPEN.find(x => x.name == "ExperimentStore").actionHandler({user: {flags: 1}, type: "CONNECTION_OPEN"})
+		} catch (e) {} // this will always intentionally throw
+		oldGetUser = userStore.default.__proto__.getCurrentUser;
+		userStore.default.__proto__.getCurrentUser = () => ({hasFlag: () => true})
+		settingsStore.default._dispatcher._orderedActionHandlers.CONNECTION_OPEN.find(x => x.name == "DeveloperExperimentStore").actionHandler()
+		userStore.default.__proto__.getCurrentUser = oldGetUser
 	}
 
 	stop(){
-		settingsExports.default = original;
+		settingsStore.default._dispatcher._orderedActionHandlers.CONNECTION_OPEN.find(x => x.name == "DeveloperExperimentStore").actionHandler()
 	}
 };
