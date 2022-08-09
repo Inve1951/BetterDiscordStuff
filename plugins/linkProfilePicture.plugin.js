ab027deacb49302390342b598228067c4d7458fd
Tiago Silva <tiagolsilva14@gmail.com>
2022-08-09 12:09:26 +0100

    linkPfp v1.2.4
    Fixed not working with latest BetterDiscord

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Link-Profile-Picture
  * @description Lets you click users' avatars on their profile page to view a bigger version in your browser.
- * @version 1.2.3
+ * @version 1.2.4
  * @author square
  * @authorLink https://betterdiscord.app/developer/square
  * @website https://betterdiscord.app/plugin/Link-Profile-Picture
@@ -11,12 +11,14 @@
 
 module.exports = class linkProfilePicture {
   start() {
-    document.addEventListener("click", LinkProfilePicture, true);
-    this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
-    function LinkProfilePicture({ target }) {
-      if (target.classList.contains("avatar-3QF_VA") && target.parentElement?.parentElement?.classList.contains("header-S26rhB")) {
-        window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
-      }
+    document.addEventListener("click", this.LinkProfilePicture, true);
+  }
+  stop() {
+    document.removeEventListener("click", this.LinkProfilePicture, true);
+  }
+  LinkProfilePicture = ({ target }) => {
+    if (target.classList.contains("avatar-3QF_VA") && target.parentElement?.parentElement?.classList.contains("header-S26rhB")) {
+      window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
     }
   }
 };
