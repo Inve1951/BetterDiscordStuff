a380417759d152edf7dd683685ca5ed78c78f015
Stefen Wakefield <me@xstefen.dev>
2022-08-21 01:21:35 -0500

    Update linkProfilePicture.plugin.js
    Satisfy current BD changes to allow loading, tested and works

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Link-Profile-Picture
  * @description Lets you click users' avatars on their profile page to view a bigger version in your browser.
- * @version 1.2.3
+ * @version 1.2.4
  * @author square
  * @authorLink https://betterdiscord.app/developer/square
  * @website https://betterdiscord.app/plugin/Link-Profile-Picture
@@ -9,14 +9,26 @@
  * @updateUrl https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/linkProfilePicture.plugin.js
  */
 
-module.exports = class linkProfilePicture {
-  start() {
-    document.addEventListener("click", LinkProfilePicture, true);
-    this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
-    function LinkProfilePicture({ target }) {
-      if (target.classList.contains("avatar-3QF_VA") && target.parentElement?.parentElement?.classList.contains("header-S26rhB")) {
-        window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
+module.exports = (_ => {
+	const config = {
+		"info": {
+			"name": "Link-Profile-Picture",
+			"author": "square",
+			"version": "1.2.4",
+			"description": "Lets you click users' avatars on their profile page to view a bigger version in your browser."
+		}
+	};
+  return class linkProfilePicture {
+    load() {
+      document.addEventListener("click", LinkProfilePicture, true);
+      this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
+      function LinkProfilePicture({ target }) {
+        if (target.classList.contains("avatar-3QF_VA") && target.parentElement?.parentElement?.classList.contains("header-S26rhB")) {
+          window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
+        }
       }
     }
-  }
-};
+    start () {this.load();}
+    stop () {}
+  };
+})();
