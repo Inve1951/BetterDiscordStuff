a8ee8ca5d78dbf21a39c4dc26dee34ec0c63a043
BadScribbles <megamanbassxx@gmail.com>
2022-08-24 01:44:57 +0100

    Link-Profile-Picture v1.2.4

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Link-Profile-Picture
  * @description Lets you click users' avatars on their profile page to view a bigger version in your browser.
- * @version 1.2.3
+ * @version 1.2.4
  * @author square
  * @authorLink https://betterdiscord.app/developer/square
  * @website https://betterdiscord.app/plugin/Link-Profile-Picture
@@ -10,6 +10,7 @@
  */
 
 module.exports = class linkProfilePicture {
+  stop(){}
   start() {
     document.addEventListener("click", LinkProfilePicture, true);
     this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
