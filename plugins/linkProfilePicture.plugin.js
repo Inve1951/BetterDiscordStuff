640ca94a06c09ee0a620931d2184494166b3ac35
square <square3880@gmail.com>
2023-09-09 21:11:30 +0200

    linkProfilePicture v1.3.0

--- plugins/linkProfilePicture.plugin.js
+++ plugins/linkProfilePicture.plugin.js
@@ -1,12 +1,12 @@
 /**
  * @name Link-Profile-Picture
  * @description Lets you click users' avatars on their profile page to view a bigger version in your browser.
- * @version 1.2.7
+ * @version 1.3.0
  * @author square
  * @authorLink https://betterdiscord.app/developer/square
  * @website https://betterdiscord.app/plugin/Link-Profile-Picture
  * @source https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/linkProfilePicture.plugin.js
- * @updateUrl https://raw.githubusercontent.com/Inve1951/BetterDiscordStuff/master/plugins/linkProfilePicture.plugin.js
+ * @updateUrl https://betterdiscord.app/gh-redirect/?id=287
  */
 
 module.exports = class linkProfilePicture {
@@ -16,7 +16,7 @@ module.exports = class linkProfilePicture {
     this.stop = document.removeEventListener.bind(document, "click", LinkProfilePicture, true);
     function LinkProfilePicture({ target }) {
       if (target.classList.contains("avatar-3QF_VA") && target.parentElement?.parentElement?.classList.contains("header-S26rhB")) {
-        window.open(target.querySelector("img").src.replace(/(?:\?size=\d{2,4})?$/, "?size=4096"), "_blank");
+        window.open(target.querySelector("img").src.replace(/\?.*$/, "?quality=lossless&size=4096"), "_blank");
       }
     }
   }
